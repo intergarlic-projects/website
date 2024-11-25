ec0afc4ed44b7ac561629e322f58caaa16acd527
AmmeJane <77190981+AmmeJane@users.noreply.github.com>
2024-11-25 20:26:28 +0000

    changed next config again, agaian

--- next.config.cjs
+++ next.config.cjs
@@ -4,7 +4,7 @@ const nextConfig = {};
 import path from 'path';
 
 nextConfig.webpack = (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    config.resolve.alias['@'] = path.resolve(__dirname, './src/src');
     return config;
 }
 
