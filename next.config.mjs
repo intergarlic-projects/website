883aad8d8df831bd4ab006bc5786a8fdf10ba0d4
AmmeJane <77190981+AmmeJane@users.noreply.github.com>
2024-11-25 20:03:52 +0000

    changed back to true ESM next config

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
